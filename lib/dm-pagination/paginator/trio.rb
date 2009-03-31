0cbcfba7ccaf6a8f22d3a880e9a83b5d4c5e0099
takiuchi <genki@s21g.com>
2009-03-31 10:39:26 +0900

    Fixed a bug and bumped version number.

--- lib/dm-pagination/paginator/trio.rb
+++ lib/dm-pagination/paginator/trio.rb
@@ -2,6 +2,7 @@ module DmPagination
   module Paginator
     class Trio < Base
       def pages(window = 5, left = 2, right = 2)
+        return [] if num_pages <= 1
         (1..num_pages).inject([]) do |result, i|
           i <= left || (num_pages - i) < right || (i-page).abs < window ?
             result << i : (result.last.nil? ? result : result << nil)
